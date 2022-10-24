b6c86862db9f52b9ec347a4677597209756a682a
Estebanfonseca <estebanp57@hotmail.com>
2022-10-24 11:16:48 -0500

    stats cambiado

--- js/stats.js
+++ js/stats.js
@@ -23,3 +23,38 @@ async function tableSingle(){
     }
 }
 tableSingle()
+
+async function tableFuture(){
+    try{
+        let table = await fetch("https://mind-hub.up.railway.app/amazing?time=upcoming")
+        table = await table.json()
+        let data = table.events.sort((a,b)=> a.category.localeCompare(b.category))
+        let category = new Set(data.map(item => item.category))
+        category = [...category]
+    }
+    catch{
+        alert("404 not found")
+    }
+}
+tableFuture()
+
+async function tablePast(){
+    try{
+        let table = await fetch("https://mind-hub.up.railway.app/amazing?time=past")
+        table = await table.json()
+        let data = table.events.sort((a,b)=> a.category.localeCompare(b.category))
+        let category = data.filter(item => item.category.toLowerCase().includes("book"))
+        let assis = category.map((item)=>{
+           return item.price * item.assistance
+        })
+        let suma = assis[0] + assis[1] + assis[2]
+
+
+        
+        
+    }
+    catch{
+        alert("404 not found")
+    }
+}
+tablePast()
